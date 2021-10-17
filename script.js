162949d44db049301508679b0db12531773374f9
Nadia K <nadiakrakowski@gmail.com>
2021-10-17 17:53:09 -0500

    error handling, getURL function

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
-//api info
+// api info
 const LASTFM_API_URL = "http://ws.audioscrobbler.com/2.0/?method=";
 const LASTFM_API_KEY = "5e7443ce206de81a5b342b82bcd4096b";
 
-//get html elements
+// get html elements
 const userTextbox = document.getElementById("user-txt");
+const displayLog = document.getElementById("log");
 const table = document.getElementById("artistTable");
 const getTopArtistsBtn = document.getElementById("get-top-artists");
 
-/*
- *array of json objects
- *stores artist objects from json returned by lastfm api requests
- */
+// stores array of json objects fetched from api
 let artistsData = [];
+
+// return
+function getURL(method, user) {
+  var url = LASTFM_API_URL +
+    method +
+    "&format=json&api_key=" +
+    LASTFM_API_KEY;
+  if (method === "user.getTopArtists") url += "&user=" + user;
+  return url;
+}
 
 // fetch top artists from last.fm charts
 async function getTopArtists() {
-  const url =
-    LASTFM_API_URL +
-    "chart.getTopArtists&api_key=" +
-    LASTFM_API_KEY +
-    "&format=json";
-  const res = await fetch(url);
-  const artistsData = await res.json();
-  addArtists(artistsData.artists);
-  updateTable();
-}
-
-//fetch top artists for user (default WojPaint; havent implemented user select)
-async function getUserTopArtists() {
-  const user = document.getElementById("user-txt").value;
-  if (!user) getTopArtists();
-  else {
-    const url =
-      LASTFM_API_URL +
-      "user.getTopArtists&api_key=" +
-      LASTFM_API_KEY +
-      "&format=json&user=" +
-      user;
-    const res = await fetch(url);
-    const artistsData = await res.json();
-    addArtists(artistsData.topartists);
+  displayLog.innerHTML = "Request processing...";
+  try {
+    const res = await fetch(getURL("chart.getTopArtists"));
+    const data = await res.json();
+    addArtists(data.artists.artist);
     updateTable();
+  }
+  catch (err) {
+    displayLog.innerHTML = "Request failed. " + err;
+    table.innerHTML="";
   }
 }
 
-/*
- *clear artists array
- *then push provided json artists to array
- */
+
+// fetch top artists for user (default WojPaint; havent implemented user select)
+async function getUserTopArtists() {
+  displayLog.innerHTML = "Request processing...";
+  try {
+    const user = document.getElementById("user-txt").value;
+    if (!user) getTopArtists();
+    else {
+      const res = await fetch(getURL("user.getTopArtists", user));
+      if(res.statusText === "Not Found") throw Error('User not found');
+      const data = await res.json();
+      addArtists(data.topartists.artist);
+      updateTable();
+    }
+  }
+  catch (err) {
+    displayLog.innerHTML = "Request failed. " + err;
+    table.innerHTML="";
+  }
+}
+
+// clear artists array, then push provided json artists to array
 function addArtists(artists) {
   artistsData = [];
-  artists.artist.forEach((artist) => {
+  artists.forEach((artist) => {
     artistsData.push(artist);
   });
 }
 
-/*
- *update table DOM to display stored artist data
- */
+// update table DOM to display stored artist data/
 function updateTable(providedData = artistsData) {
+  displayLog.innerHTML = "";
   //clear table and add a header row
   var headRow = `
   <tr class="info">
